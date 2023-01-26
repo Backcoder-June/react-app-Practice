3fd0b0c564ae417031535279c485a8b71482bdf7
backcdoer <backcoder2@gmail.com>
2023-01-26 12:09:34 +0900

    header_footer_applied

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{
+        width: '100%', 
+        padding: '50px 30px', 
+        display : 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '24px',
+        background: 'silver', 
+        color: '#fff', 
+        marginTop: '100px'
+    }}>copyright. 저작권은 없습니다. </footer>
+  )
+}
+
+export default Footer
